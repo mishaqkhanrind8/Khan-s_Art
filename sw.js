96e587c22f21030f096c1b872fb7cbc0f6db5994
Muhammad Ishaq Khan <aonemobilesncomputers786@gmail.com>
2025-08-21 08:43:58 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
 // Service Worker file
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('ppl-cache-v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        '/styles.css',
+        '/script.js',
+        '/assets/android-chrome-192x192.png',
+        '/assets/android-chrome-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
